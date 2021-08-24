b795ef9c386651d021ffce0bd4bbe7ae9950e81b
rmnaser90 <rmnaser90@gmail.com>
2021-08-24 14:43:01 +0300

    removing unneccery code

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const path = require('path')
 const api = require('./server/routes/api')
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useFindAndModify: true })
-const City = require('./server/models/city.js')
-const router = require('./server/routes/api')
+
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
     next()
 })
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'dist')))
